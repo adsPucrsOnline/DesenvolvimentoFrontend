df455ea113950f22537d3f42f2fd57f4286fb000
Bianca Camargo Machado <bianca.camargo@acad.pucrs.br>
2025-04-04 22:15:37 +0100

    Update pwa-clima API Key info

---
+++ aula-09/exemplos/pwa-clima/src/util/constantes/index.js
@@ -0,0 +1,4 @@
+// Você deve criar a sua conta e obter a chave de API no site https://home.openweathermap.org/users/sign_in
+// Após login, acesse esta página para copiar a API Key: https://home.openweathermap.org/api_keys
+// A API Key pode levar algumas horas para ser ativada, então é normal se não conseguir usar logo após a criação da conta.
+export const KEY_API_CLIMA = ""
